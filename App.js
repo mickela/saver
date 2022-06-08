c5a2d36079fc404ca8cd47f0748ffb36640b8661
mickela <osoba.michael@outlook.com>
2022-06-08 17:52:46 +0100

    Add stack navigator

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, View, Text } from 'react-native';
 import WhatsApp from './Screens/WhatsApp';
+import WhatsAppBusiness from './Screens/WhatsAppBusiness';
+
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>WhatsApp Status Saver</Text>
-      </View>
-
-      <WhatsApp />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="WhatsAppBusiness" component={WhatsAppBusiness} options={{
+          title: "WhatsApp Business",
+          headerStyle: {
+            backgroundColor: "#f4511e",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: 'bold'
+          }
+        }} />
+        <Stack.Screen name="WhatsApp" component={WhatsApp} options />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
