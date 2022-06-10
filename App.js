5dd075628a2da7d84942f59610fd1c04abe0fb21
mickela <osoba.michael@outlook.com>
2022-06-10 19:06:53 +0100

    Implement full tab naviation

--- App.js
+++ App.js
@@ -2,14 +2,9 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, View, Text, Image } from 'react-native';
-import WhatsApp from './Screens/WhatsApp';
-import WhatsAppBusiness from './Screens/WhatsAppBusiness';
+import ImageView from './Screens/ImageView';
 import { StatusBar } from 'expo-status-bar';
-
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-const Tab = createBottomTabNavigator();
-
+import Tabs from './Tabs';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -23,62 +18,15 @@ export default function App() {
     }
   };
 
-  const TabBarIcon = ({focused, img}) => {
-    let image = img == 'business' ? require(`./assets/whatsapp-business.png`) : require(`./assets/whatsapp.png`);
-    
-    return (
-    <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-      <Image
-          source={image}
-          resizeMode="contain"
-          style={{
-              width: focused ? 60 : 40 ,
-              height: focused ? 60 : 40,
-              marginBottom: focused ? 30 : 0,
-          }}
-      />
-    </View>
-  )};
-
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator>
-        <Stack.Screen name="WhatsAppBusiness" component={WhatsAppBusiness} options={{
-          title: "WhatsApp Business",
+      <Stack.Navigator initialRouteName={'Tabs'}>
+        <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }}/>
+        <Stack.Screen name="Image" component={ImageView} options={{
+          title: "Image View",
           ...headerStyles
         }} />
-        <Stack.Screen name="WhatsApp" component={WhatsApp} options={{
-          ...headerStyles
-        }} />
-      </Stack.Navigator> */}
-      <Tab.Navigator
-        screenOptions={{
-          tabBarShowLabel: false,
-          style: {
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              right: 0,
-              elevation: 0,
-              backgroundColor: "red",
-              borderTopColor: "transparent",
-              height: 100,
-          },
-          // tabBarBackground: {
-
-          // },
-          headerStyle: {
-            backgroundColor: "#f4511e",
-          },
-          headerTintColor: "#fff"
-      }}
-    >
-        <Tab.Screen name="WhatsApp" component={WhatsApp} options={{
-          tabBarIcon: ({focused}) => <TabBarIcon focused={focused} img="personal" /> }} />
-
-        <Tab.Screen name="WhatsAppBusiness" component={WhatsAppBusiness} options={{
-          tabBarIcon: ({focused}) => <TabBarIcon focused={focused} img="business" /> }} />
-      </Tab.Navigator>
+      </Stack.Navigator>
       <StatusBar style="light" />
     </NavigationContainer>
   );
---
+++ Tabs.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import WhatsApp from './Screens/WhatsApp';
+import WhatsAppBusiness from './Screens/WhatsAppBusiness';
+
+const Tab = createBottomTabNavigator();
+
+
+export const Tabs = () =>{
+    const TabBarIcon = ({focused, img}) => {
+        let image = img == 'business' ? require(`./assets/whatsapp-business.png`) : require(`./assets/whatsapp.png`);
+        
+        return (
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Image
+              source={image}
+              resizeMode="contain"
+              style={{
+                  width: focused ? 60 : 40 ,
+                  height: focused ? 60 : 40,
+                  marginBottom: focused ? 30 : 0,
+              }}
+          />
+        </View>
+    )};
+
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarShowLabel: false,
+                style: {
+                    position: "absolute",
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    elevation: 0,
+                    backgroundColor: "red",
+                    borderTopColor: "transparent",
+                    height: 100,
+                },
+                // tabBarBackground: {
+
+                // },
+                headerStyle: {
+                    backgroundColor: "#f4511e",
+                },
+                headerTintColor: "#fff"
+            }}
+        >
+            <Tab.Screen name="WhatsApp" component={WhatsApp} options={{
+            tabBarIcon: ({focused}) => <TabBarIcon focused={focused} img="personal" /> }} />
+
+            <Tab.Screen name="WhatsApp Business" component={WhatsAppBusiness} options={{
+            tabBarIcon: ({focused}) => <TabBarIcon focused={focused} img="business" /> }} />
+        </Tab.Navigator>
+    )
+  
+}
+
+export default Tabs
